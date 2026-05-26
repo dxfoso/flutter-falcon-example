e0302c30a30a5452c93986d0c58ff1c9b8109736
Big One <dxfoso@gmail.com>
2026-05-26 15:37:15 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class FalconFixturePage extends StatelessWidget {
                   height: 180,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.black,
                     borderRadius: BorderRadius.circular(24),
                   ),
                   child: Text(
