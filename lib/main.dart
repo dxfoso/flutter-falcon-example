c5311faadc72b0f7816e42713956b944557dd3b0
Big One <dxfoso@gmail.com>
2026-06-01 08:50:59 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class _FalconVersionPageState extends State<FalconVersionPage> {
     return Scaffold(
       backgroundColor: const Color(0xFFF6F1EE),
       appBar: AppBar(
-        title: const Text('Flutter Falcon Version'),
+        title: const Text('Flutter Falcon Version sssss'),
         actions: [
           IconButton(onPressed: _refresh, icon: const Icon(Icons.refresh)),
         ],
