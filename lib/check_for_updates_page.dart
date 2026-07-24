2ad6cd316e8e867e7cb035590b534c91b1751fee
Big One <dxfoso@gmail.com>
2026-07-24 16:01:25 +0200

    fdfdwsaf

--- lib/check_for_updates_page.dart
+++ lib/check_for_updates_page.dart
@@ -14,7 +14,7 @@ _UpdateMessageText _updateMessageText(
   BuildContext context,
   FlutterFalconUpdatePresentation presentation,
 ) {
-  int s = 2;
+  int s = 3;
   final arabic = Localizations.localeOf(context).languageCode == 'ar';
   if (!arabic) {
     return _UpdateMessageText(
