91f69c24ffb5d951c01ac5881e25c7e244c15ba7
Big One <dxfoso@gmail.com>
2026-08-04 00:57:19 +0200

    dsd

--- lib/check_for_updates_page.dart
+++ lib/check_for_updates_page.dart
@@ -119,7 +119,7 @@ class _CheckForUpdatesPageState extends State<CheckForUpdatesPage> {
     final state = _visibleState;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Check for updates'),
+        title: const Text('1 Check for updates'),
         actions: [
           IconButton(
             key: const Key('check-updates-icon-button'),
