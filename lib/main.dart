55485875aab4769f9306760a8a8db17ed226e8f6
Big One <dxfoso@gmail.com>
2026-08-28 22:28:03 +0200

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class FlutterFalconExampleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: '101 FlutterFalcon Example',
+        title: '101.. FlutterFalcon Example',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
